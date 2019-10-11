3c6bc798096a8f2b275ccff3853004109ca8b786
Robert Keffury <bobby.keffury@me.com>
2019-10-10 18:53:54 -0700

    Finished EntryDetailViewController implementation.

---
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Bobby Keffury on 10/10/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+}
